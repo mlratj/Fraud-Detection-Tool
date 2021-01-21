80e5debd9dccf88d87df0ae7be7dc8520ba2f35d
mlrtj <ml.ratajewski@gmail.com>
2021-01-21 20:48:58 +0100

    Adding Benford distribution generator.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import os
+from math import log
 
 
-def main(datasource_name):
+def main(datasource_name, column_name):
     source_path = os.path.dirname(__file__)
     data = pd.read_csv(source_path + '/datasource/' + datasource_name + '.csv')
     return None
@@ -15,6 +16,16 @@ def extension_checker(filename):
         return filename + '.csv'
 
 
+def benford_distribution():
+    benford = []
+    i = 1
+    while i < 10:
+        val = log(1 + 1/i, 10)
+        benford.append(val)
+        i += 1
+    return benford
+
 if __name__ == "__main__":
-    file_to_check = input("Please type in a CSV file to check: ")
-    main(extension_checker(file_to_check))
+    file_to_check = input("Please type in a CSV file to check:\n ")
+    field_to_check = input("Please specify a field to be checked by the tool:\n")
+    main(extension_checker(file_to_check), field_to_check)
