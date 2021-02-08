c9b7c19fdfa1c3a58eacea5a57900cf292c6adb9
mlrtj <ml.ratajewski@gmail.com>
2021-02-08 19:17:19 +0100

    Implementing an option to proceed a file from the web

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pandas as pd
-import os
+import io
+import requests
 import numpy as np
 from math import log, floor
 from matplotlib import pyplot as plt
+from extras import menu
+from extras import checkers
 
 
-def main(column_name):
+def main(valid_file):
+    print("Data loaded successfully")
+    column_name = ''
+    while len(column_name) < 1:
+        column_name = input("Please specify a field to be checked by the tool:\n")
     try:
-        df = data_load(file_to_check)
-    except FileNotFoundError:
-        print("No such file.")
-        raise SystemExit
-    try:
-      df['first_d'] = df[column_name].apply(lambda x: first_digit(x))
-      first_digits = list(df['first_d'])
-      first_digits.remove(0)
+        df = pd.read_csv(valid_file)
+        df['first_d'] = df[column_name].apply(lambda x: first_digit(x))
+        first_digits = list(df['first_d'])
     except KeyError:
         print("Provided column name doesn't exists.")
         raise SystemExit
+    try:
+        first_digits.remove(0)
+    except ValueError:
+        pass
     try:
         first_digits_set = set(first_digits)
         input_occ = occurrence_count(first_digits_set, first_digits)
@@ -29,17 +35,25 @@ def main(column_name):
     return None
 
 
-def data_load(datasource_name):
-    source_path = os.path.dirname(os.path.abspath(__file__))
-    data = pd.read_csv(source_path + '/datasource/' + datasource_name + '.csv')
-    return data
-
-
-def extension_checker(filename):
-    if filename.lower().endswith('.csv'):
-        return filename
+def data_source(option):
+    if option == 1:
+        # option 1:
+        file_to_check = input("Please type in a name of CSV file to check:\n")
+        valid_file = checkers.extension_checker(file_to_check)
+        checkers.file_search(valid_file)
+        try:
+            df = checkers.data_load(valid_file)
+        except FileNotFoundError:
+            print("No such file.")
+            raise SystemExit
     else:
-        return filename + '.csv'
+        # option 2:
+        url = input("Please enter an URL to the CSV file to check. \n")
+        print("\n Loading...")
+        s = requests.get(url).content
+        s_io = io.StringIO(s.decode('utf-8'))
+        df = s_io
+    return df
 
 
 def benford_distribution():
@@ -68,19 +82,19 @@ def occurrence_count(data_set, data_list):
 
 def percentage_of_total(l):
     total = sum(l)
-    perc_list = [(x / total)*100 for x in l]
+    perc_list = [(x / total) * 100 for x in l]
     return perc_list
 
 
-def draw_histogram(benford, user_data = None):
+def draw_histogram(benford, user_data=None):
     """Creates a data plot that allows to check if data provided by a user meets rules discovered by Frank Benford."""
     column_width = 0.4
     labels = []
-    for x in range(1,10):
+    for x in range(1, 10):
         labels.append(x)
     index = np.arange(len(labels))
-    plt.bar(index - column_width/2, benford, column_width, label = 'Benford',  color = 'green')
-    plt.bar(index + column_width/2, user_data, column_width, label = 'Data provided', color = 'red')
+    plt.bar(index - column_width / 2, benford, column_width, label='Benford', color='green')
+    plt.bar(index + column_width / 2, user_data, column_width, label='Data provided', color='red')
     plt.xticks(index, labels)
     plt.title("Benford distribution vs provided data")
     plt.xlabel("First digits")
@@ -90,8 +104,7 @@ def draw_histogram(benford, user_data = None):
 
 
 if __name__ == "__main__":
-    file_to_check = input("Please type in a CSV file to check:\n")
-    field_to_check = input("Please specify a field to be checked by the tool:\n")
-    valid_file = extension_checker(file_to_check)
-    main(field_to_check)
+    load_choice = menu.print_menu()
+    loaded_df = data_source(load_choice)
+    main(loaded_df)
     # example parameters: 1. hydrology_areas 2.Shape__Area
